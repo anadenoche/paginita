bec4d881cbb5c3f26f2d8ffb5ed2bc12d28f598c
Ana Freidenberg <anafreidenberg@gmail.com>
2022-11-12 11:23:37 -0300

    listo

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let nombre = prompt("QUIÉN SOS")
+let estado = prompt("COMO ANDAS?")
+
+
+
+if (estado = "bien") {
+    let mensaje = prompt("Hola, " + nombre + ". Qué bueno que estés bien, yo también.")
+}
+else {
+    let mensaje2 = prompt("Hola, " + nombre + ". Tranqui, a veces es así.")
+    
+}
